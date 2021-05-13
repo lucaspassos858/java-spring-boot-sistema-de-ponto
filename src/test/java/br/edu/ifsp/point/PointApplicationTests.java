51e27dfd301013f9b166e62fe8c4c96a81efdcb1
Bruno Silva <brunosilva2365@gmail.com>
2021-05-12 23:27:52 -0300

    Início do projeto

---
+++ src/test/java/br/edu/ifsp/point/PointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8339c2958a9e221165c6b3a9227832f55c080aed
Bruno Silva <brunosilva2365@gmail.com>
2021-05-13 23:00:26 -0300

    Operações do usuário

--- src/main/java/br/edu/ifsp/point/controllers/UserController.java
+++ src/main/java/br/edu/ifsp/point/controllers/UserController.java
@@ -3,10 +3,13 @@ package br.edu.ifsp.point.controllers;
 import br.edu.ifsp.point.models.User;
 import br.edu.ifsp.point.models.api.SuccessResponse;
 import br.edu.ifsp.point.services.UserService;
+import br.edu.ifsp.point.utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -17,27 +20,42 @@ public class UserController {
 
     @GetMapping("")
     public ResponseEntity<SuccessResponse> findAll(){
-        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "FindAll"));
+
+        List<User> users = userService.findAll();
+
+        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, users));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse> findById(@PathVariable("id") Long id){
-        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "FindById => " + id));
+
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, user));
     }
 
     @PostMapping("")
-    public ResponseEntity<SuccessResponse> create(@RequestBody User user){
-        return ResponseEntity.status(201).body(new SuccessResponse(HttpStatus.CREATED, "CreateUser"));
+    public ResponseEntity<SuccessResponse> save(@RequestBody User user){
+
+        User userCreated = userService.save(user);
+
+        return ResponseEntity.status(201).body(new SuccessResponse(HttpStatus.CREATED, userCreated));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<SuccessResponse> update(@PathVariable("id") Long id, @RequestBody User user){
-        return ResponseEntity.ok().body(new SuccessResponse(HttpStatus.OK, "UpdateUser => " + id));
+
+        User userUpdated = userService.update(id, user);
+
+        return ResponseEntity.ok().body(new SuccessResponse(HttpStatus.OK, userUpdated));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<SuccessResponse> delete(@PathVariable("id") Long id){
-        return ResponseEntity.status(204).body(new SuccessResponse(HttpStatus.NO_CONTENT, "DeleteUser => " + id));
+    public ResponseEntity<SuccessResponse> deleteById(@PathVariable("id") Long id){
+
+        userService.deleteById(id);
+
+        return ResponseEntity.status(204).body(new SuccessResponse(HttpStatus.NO_CONTENT, "Usuário deletado com sucesso"));
     }
 
 }
--- src/main/java/br/edu/ifsp/point/services/UserService.java
+++ src/main/java/br/edu/ifsp/point/services/UserService.java
@@ -1,13 +1,44 @@
 package br.edu.ifsp.point.services;
 
+import br.edu.ifsp.point.exceptions.handler.UserNotFoundException;
+import br.edu.ifsp.point.models.User;
 import br.edu.ifsp.point.repositories.UserRepository;
+import br.edu.ifsp.point.utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Usuário não encontrado na base de dados"));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User update(Long id, User user) {
+
+        User userFound = findById(id);
+
+        return userRepository.save(userFound);
+    }
+
+    public void deleteById(Long id) {
+
+        findById(id);
+
+        userRepository.deleteById(id);
+    }
 
 }

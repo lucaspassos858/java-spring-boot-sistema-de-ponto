13f82da64173b1d043e327a1220954bddd73e6ea
Bruno Silva <brunosilva2365@gmail.com>
2021-05-23 12:58:13 -0300

    Operações do sistema de ponto

--- src/main/java/br/edu/ifsp/point/controllers/TimesheetController.java
+++ src/main/java/br/edu/ifsp/point/controllers/TimesheetController.java
@@ -2,11 +2,14 @@ package br.edu.ifsp.point.controllers;
 
 import br.edu.ifsp.point.models.Timesheet;
 import br.edu.ifsp.point.models.api.SuccessResponse;
+import br.edu.ifsp.point.models.dtos.TimesheetDTO;
 import br.edu.ifsp.point.services.TimesheetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/timesheet")
@@ -17,27 +20,59 @@ public class TimesheetController {
 
     @GetMapping("")
     public ResponseEntity<SuccessResponse> findAll(){
-        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "FindAll"));
+
+        List<Timesheet> timesheets = timesheetService.findAll();
+
+        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, timesheets));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse> findById(@PathVariable("id") Long id){
-        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, "FindById => " + id));
+
+        Timesheet timesheet = timesheetService.findById(id);
+
+        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, timesheet));
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<SuccessResponse> findByUserId(@PathVariable("id") Long id){
+
+        List<Timesheet> timesheets = timesheetService.findByUserId(id);
+
+
+        return ResponseEntity.ok(new SuccessResponse(HttpStatus.OK, timesheets));
     }
 
     @PostMapping("")
-    public ResponseEntity<SuccessResponse> create(@RequestBody Timesheet timesheet){
-        return ResponseEntity.status(201).body(new SuccessResponse(HttpStatus.CREATED, "CreateTimesheet"));
+    public ResponseEntity<SuccessResponse> create(@RequestBody TimesheetDTO obj){
+
+        Timesheet timesheet = timesheetService.save(obj);
+
+        return ResponseEntity.status(201).body(new SuccessResponse(HttpStatus.CREATED, timesheet));
+    }
+
+    @PostMapping("/submit")
+    public ResponseEntity<SuccessResponse> submit(@RequestBody TimesheetDTO obj){
+
+        Timesheet timesheet = timesheetService.save(obj);
+
+        return ResponseEntity.status(201).body(new SuccessResponse(HttpStatus.CREATED, timesheet));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<SuccessResponse> update(@PathVariable("id") Long id, @RequestBody Timesheet timesheet){
-        return ResponseEntity.ok().body(new SuccessResponse(HttpStatus.OK, "UpdateTimesheet => " + id));
+    public ResponseEntity<SuccessResponse> update(@PathVariable("id") Long id, @RequestBody TimesheetDTO obj){
+
+        Timesheet timesheet = timesheetService.update(id, obj);
+
+        return ResponseEntity.ok().body(new SuccessResponse(HttpStatus.OK, timesheet));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<SuccessResponse> delete(@PathVariable("id") Long id){
-        return ResponseEntity.status(204).body(new SuccessResponse(HttpStatus.NO_CONTENT, "DeleteTimesheet => " + id));
+
+        timesheetService.deleteById(id);
+
+        return ResponseEntity.status(204).body(new SuccessResponse(HttpStatus.NO_CONTENT, "Apontamento de horas deletado com sucesso"));
     }
 
 }
--- src/main/java/br/edu/ifsp/point/services/TimesheetService.java
+++ src/main/java/br/edu/ifsp/point/services/TimesheetService.java
@@ -1,13 +1,77 @@
 package br.edu.ifsp.point.services;
 
+import br.edu.ifsp.point.exceptions.handler.UserNotFoundException;
+import br.edu.ifsp.point.models.Timesheet;
+import br.edu.ifsp.point.models.User;
+import br.edu.ifsp.point.models.dtos.TimesheetDTO;
 import br.edu.ifsp.point.repositories.TimesheetRepository;
+import br.edu.ifsp.point.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TimesheetService {
 
     @Autowired
     TimesheetRepository timesheetRepository;
+
+    @Autowired
+    UserService userService;
+
+    public List<Timesheet> findAll(){
+        return timesheetRepository.findAll();
+    }
+
+    public Timesheet findById(Long id){
+        return timesheetRepository.findById(id).orElseThrow(() -> new UserNotFoundException("Apontamento de horas não encontrado na base de dados"));
+    }
+
+    public List<Timesheet> findByUserId(Long id){
+        return timesheetRepository.findByUserId(id);
+    }
+
+    public Timesheet save(TimesheetDTO timesheetDTO) {
+
+        Timesheet timesheet = this.normalize(null, timesheetDTO);
+
+        return timesheetRepository.save(timesheet);
+    }
+
+    public Timesheet update(Long id, TimesheetDTO timesheetDTO) {
+
+        Timesheet timesheet = this.normalize(id, timesheetDTO);
+
+        return timesheetRepository.save(timesheet);
+    }
+
+    public void deleteById(Long id) {
+
+        findById(id);
+
+        timesheetRepository.deleteById(id);
+    }
+
+    private Timesheet normalize(Long id, TimesheetDTO timesheetDTO){
+
+        Timesheet timesheet;
+
+        if(id == null) {
+            timesheet = new Timesheet();
+        } else {
+            timesheet = this.findById(id);
+        }
+
+        User user = userService.findById(timesheetDTO.getUserId());
+
+        timesheet.setDate(timesheetDTO.getDate());
+        timesheet.setStart(timesheetDTO.getStart());
+        timesheet.setEnd(timesheetDTO.getEnd());
+        timesheet.setUser(user);
+
+        return timesheet;
+
+    }
 
 }

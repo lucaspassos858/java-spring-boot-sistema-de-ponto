bbafa995b3799be3e992da81705e4e63c010b5ee
guilhermect <guilherme.tavares@saint-gobain.com>
2021-05-25 23:19:44 -0300

    Alterar título e placeholder

--- sistema-de-pontos/src/index.js
+++ sistema-de-pontos/src/index.js
@@ -13,22 +13,24 @@ function App() {
     console.log('Nome => ' + data.name);
     console.log('Hora de Entrada => ' + data.horaEntrada);
     console.log('Hora de Saída => ' + data.horaSaida);
+
+    alert(data.name + '\n' + data.horaEntrada + '\n' + data.horaSaida);
   };
 
   return (
     <div>
       <div>
-        <h1> Bem Vindo(a) ao Sistema de Pontos </h1>
+        <h1> Bem Vindo(a) ao Sistema de Ponto </h1>
       </div>
       <div style={{border:"1px solid gray", padding:"20px", width:"40%", margin:"0 auto"}}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <label> Nome </label>
-          <input {...register("name")} required/>
+          <input {...register("name")} placeholder="Digite seu nome" required/>
           <label> Hora de Entrada </label>
           <input {...register("horaEntrada")} type="time" required/>
           <label> Hora de Saída: </label>
           <input {...register("horaSaida")} type="time" required/>
-          <input type="submit" />
+          <input type="submit" value="Registrar"/>
         </form>
       </div>
     </div>
